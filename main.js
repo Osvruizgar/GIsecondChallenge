b3b1ce953f16a98538cf5b3383fc76392ca28a88
Osvaldo <osvruizgar@gmail.com>
2022-04-25 21:22:26 -0400

    completed the very easy part of the challenge however I kept over thinking about how I want to code. I picked the bottom code.

---
+++ main.js
@@ -0,0 +1,14 @@
+//very easy
+// function min(firstNumb, secondNum) {
+//   const smallest = firstNumb < secondNum ? firstNumb : secondNum;
+//   console.log(smallest);
+// }
+// min(6, 4);
+// I like the bottom code more but Im keeping the top one to review when I come back.
+//very easy
+function min(firstNumb, secondNum) {
+    return smallest = firstNumb < secondNum ? firstNumb : secondNum;
+  }
+  console.log(min(6, 4));
+
+//EASY
