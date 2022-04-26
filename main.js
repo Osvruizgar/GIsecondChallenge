3500d0fb3c3340adc1feafac9f74e7f6921fed05
Osvaldo <osvruizgar@gmail.com>
2022-04-25 21:40:11 -0400

    completed the easy part of the challenge and I am adding comments to myself but my instructors are going to read it so Ill come back and edit them for myself later'

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ function min(firstNumb, secondNum) {
   console.log(min(6, 4));
 
 //EASY
+const firstStudent = ["Slim", "Shady", 15];
+const seconStudent = ["Dr.","Dre", 17];
+const thirdStudent = ["50","Cent", 16];
+const gClass = [firstStudent, seconStudent, thirdStudent];
+console.log(`Hello, my name is ${gClass[0][0]} ${gClass[0][1]} and I'm ${gClass[0][2]} years old.`)
+// theres probably a shorter version to knock this out.. but I can explain this to myself.
+
+//Medium
+//don't want to do the easy way that I know will work. I want to try something different.
